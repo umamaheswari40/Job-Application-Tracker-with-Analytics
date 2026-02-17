587d22ff52481189ab04d5cf1f864bf1cbd0ee23
22K61A1440 Pulla Umamaheswari <149062675+umamaheswari40@users.noreply.github.com>
2026-02-17 21:27:35 +0530

    Create app/models/__init__.py file

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.user import User
+from app.models.application import JobApplication
+from app.models.status_history import StatusHistory
+
+__all__ = ['User', 'JobApplication', 'StatusHistory']
